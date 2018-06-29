3c6101c3a987f4239704b10589fb185a590b658d
Abhishek Amin <abhishek.amin@rapidops.com>
2018-06-29 19:34:50 +0530

    Initial Commit

--- ageDropdown.js
+++
@@ -1,3 +0,0 @@
-for (let i = 18; i < 99; i++) {
-    document.write('<option value =' + i + '>' + i + '</option>');
-}
--- app.js
+++ app.js
@@ -1,102 +1,171 @@
+this.page = 1;
+let limit = 2;
 
 let userList = [];
 
+function getUsers() {
+	document.getElementById('showPageNumber').innerHTML = `Page: <bold>${this.page}</bold>`;
+	return fetch(`http://localhost:5000/api/user?limit=${limit}&page=${this.page}`, {
+		mode: 'cors',
+		method: 'GET',
+		header: {
+			'Content-type': 'application/json',
+			'Access-Control-Allow-Headers' : '*',
+			'Access-Control-Allow-Origin' : '*'
+		}
+		})
+		.then((response) => { return response.json() })
+		.then((data) => addUser(data))
+		.catch(function(e) {
+			console.log(e);
+		});
+}
+
 // add new user to the list
-function addUser () {
-    let userFirstName = document.getElementById('firstName').value;
-    let userAge = document.getElementById('ageDropdown').value;
-    let userID = '_' + Math.random().toString(36).substr(2, 9);
+function addUser (response) {
+	userList = [];
+	for (let i = 0; i < limit; i++) {
+    // let div = document.createElement('DIV');
+    // div.setAttribute('id', i);
+    // div.innerHTML = `${response.docs[i]._id} ${response.docs[i].name} ${response.docs[i].age}`;
+    // // console.log(response.docs[i]);
+    // appDiv = document.getElementById('appDiv');
+		// appDiv.appendChild(div);
 
-    let userDetails = {
-        'firstname' : userFirstName,
-        'age' : userAge,
-        'id' : userID
-    };
-
-    new User(userDetails);
+		let userFirstName = response.docs[i].name;
+		let userAge = response.docs[i].age;
+		let userID = response.docs[i]._id;
+	
+		let userDetails = {
+				'firstname' : userFirstName,
+				'age' : userAge,
+				'id' : userID
+		};
+	
+		new User(userDetails);
+	}
+	setPageDiv(response.total / limit);
 }
 
 class User {
-    constructor(data) {
-        this.userData = data;
-    }
+		constructor(data) {
+				this.userData = data;
+		}
 
-    get userData() {
-        return this.data;
-    }
+		get userData() {
+				return this.data;
+		}
 
-    set userData(data) {
-        this.firstname = data.firstname;
-        this.age = data.age;
-        this.id = data.id;
+		set userData(data) {
+				this.firstname = data.firstname;
+				this.age = data.age;
+				this.id = data.id;
 
-        userList.push(data);
-        console.log(userList);
-        updateTable();
-    }
+				userList.push(data);
+				updateTable();
+		}
 }
 
 // function to create table
 function createTable() {
-    // create table elements
-    let table = document.createElement('TABLE');
-    let tableRow = document.createElement('TR');
-    let nameHeader = document.createElement('TH');
-    let ageHeader = document.createElement('TH');
+		// create table elements
+		let table = document.createElement('TABLE');
+		let tableRow = document.createElement('TR');
+		let nameHeader = document.createElement('TH');
+		let ageHeader = document.createElement('TH');
+		let idHeader = document.createElement('TH');
 
-    // assign id attributes
-    table.setAttribute('id', 'usersTable');
-    tableRow.setAttribute('id', 'headerRow');
-    nameHeader.setAttribute('id', 'nameHeader');
-    ageHeader.setAttribute('id', 'ageHeader');
+		// assign id attributes
+		table.setAttribute('id', 'usersTable');
+		tableRow.setAttribute('id', 'headerRow');
+		nameHeader.setAttribute('id', 'nameHeader');
+		ageHeader.setAttribute('id', 'ageHeader');
+		idHeader.setAttribute('id', 'ID');
 
-    // fill header values
-    nameHeader.innerHTML = 'First Name';
-    ageHeader.innerHTML = 'Age';
+		// fill header values
+		nameHeader.innerHTML = 'Name';
+		ageHeader.innerHTML = 'Age';
+		idHeader.innerHTML = 'Id';
 
-    // append elements
-    tableRow.appendChild(nameHeader);
-    tableRow.appendChild(ageHeader);
-    table.appendChild(tableRow);
-    document.body.appendChild(table);
+		// append elements
+		tableRow.appendChild(idHeader);
+		tableRow.appendChild(nameHeader);
+		tableRow.appendChild(ageHeader);
+
+		table.appendChild(tableRow);
+		document.getElementById('container').appendChild(table);
 }
 
 // fill or update table
 function updateTable () {
-    if (userList.length == 0) return;
+		if (userList.length == 0) return;
 
-    let tableExists = document.getElementById('usersTable');
-    
-    if (tableExists) {
-        tableExists.remove(tableExists);
-    }
+		let tableExists = document.getElementById('usersTable');
+		
+		if (tableExists) {
+				tableExists.remove(tableExists);
+		}
 
-    createTable();
-    
-    for (let i = 0; i < userList.length; i++) {
-        userRow = document.createElement('TR');
-        tableDataName = document.createElement('TD');
-        tableDataAge = document.createElement('TD');
+		createTable();
+		
+		for (let i = 0; i < userList.length; i++) {
+				userRow = document.createElement('TR');
+				tableDataName = document.createElement('TD');
+				tableDataAge = document.createElement('TD');
+				tableDataId = document.createElement('TD');
 
-        userRow.setAttribute('id', userList[i].id);
-        userRow.setAttribute('class', 'dataRow');
-        userRow.setAttribute('onclick', 'deleteUser(this)');
+				userRow.setAttribute('id', userList[i].id);
+				userRow.setAttribute('class', 'dataRow');
+				// userRow.setAttribute('onclick', 'deleteUser(this)');
 
-        tableDataName.innerHTML = userList[i].firstname;
-        tableDataAge.innerHTML = userList[i].age;
+				tableDataId.innerHTML = userList[i].id;
+				tableDataName.innerHTML = userList[i].firstname;
+				tableDataAge.innerHTML = userList[i].age;
 
-        userRow.appendChild(tableDataName);
-        userRow.appendChild(tableDataAge);
-        document.getElementById('usersTable').appendChild(userRow);
-    }
+				userRow.appendChild(tableDataId);
+				userRow.appendChild(tableDataName);
+				userRow.appendChild(tableDataAge);
+
+				document.getElementById('usersTable').appendChild(userRow);
+		}
 }
+
+function setPageDiv (page) {
+	let pageNav = document.getElementById('pageNav');
+	if(pageNav) {
+		pageNav.remove(pageNav);
+	}
+
+	pageNav = document.createElement('UL');
+	pageNav.setAttribute('id', 'pageNav');
+	document.getElementById('container').appendChild(pageNav);
+
+	for (let i = 0; i < page; i++) {
+		let pageButtonItem = document.createElement('LI');
+		let pageButton = document.createElement('button');
+
+		pageButton.value = i + 1;
+		pageButton.innerHTML = i + 1;
+		pageButton.setAttribute('onClick', 'changePage(this)');
+
+		pageButtonItem.appendChild(pageButton);
+		pageNav.appendChild(pageButtonItem);
+	}
+}
+
+function changePage(page) {
+	this.page = page.value;
+	getUsers();
+}
+
+getUsers();
 
 // function to delete user
-function deleteUser (rowToDelete) {
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i].id === rowToDelete.id) {
-            userList.splice(i, 1);
-        }
-    }
-    updateTable();
-}
+// function deleteUser (rowToDelete) {
+// 		for (let i = 0; i < userList.length; i++) {
+// 				if (userList[i].id === rowToDelete.id) {
+// 						userList.splice(i, 1);
+// 				}
+// 		}
+// 		updateTable();
+// }
